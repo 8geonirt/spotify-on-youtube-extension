52fa4bc4924c3cf4c59a4503f9fe8348ea557d5f
José Trinidad Espinoza González <trinoeg8@gmail.com>
2020-05-10 20:10:33 -0500

    Add FontAwesome Free
    - Adds dependencies to show Youtube & Spotify icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 /* global chrome */
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import './App.scss';
 import {
   sanitizeTitle,
@@ -19,6 +24,8 @@ const INITIAL_STATE = {
   message: DEFAULT_TITLE,
   messageType: 'success'
 };
+
+library.add(fab, faCheckSquare, faCoffee);
 
 class App extends Component {
   constructor(props) {
@@ -336,7 +343,9 @@ class App extends Component {
 
     return (
       <div className={`message-container ${messageType}`}>
+        <FontAwesomeIcon icon={['fab', 'spotify']} />
         {message}
+        <FontAwesomeIcon icon={['fab', 'youtube']} />
       </div>
     );
   }
